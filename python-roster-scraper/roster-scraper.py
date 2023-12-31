72de87c76fee8799335d3e323a6b0f12b8c32e4a
lukezann <luke.zanuck@gmail.com>
2023-12-30 22:01:54 -0800

    Scrape name and hometown data of Williams Men's XC roster page.

--- python-roster-scraper/roster-scraper.py
+++ python-roster-scraper/roster-scraper.py
@@ -1,4 +1,29 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-import time
+
+names = []
+hometowns = []
+
+# get request to roster page
+html = requests.get('https://ephsports.williams.edu/sports/mens-cross-country/roster').text
+soup = BeautifulSoup(html, 'html.parser')
+
+# find all athletes' data contained in lists
+athletes = soup.find_all('li', class_='sidearm-roster-player')
+
+# iterate thru and extract relevant info
+for ath in athletes:
+    name = ath.find('div', class_='sidearm-roster-player-name')
+    name = name.find('a').text.strip()
+    names.append(name)
+
+    hometown = ath.find('span', class_='sidearm-roster-player-hometown').text.strip()
+    hometowns.append(hometown)
+
+# create pandas DataFrame and export to csv
+data = {'name' : names,
+        'hometown' : hometowns};
+
+df = pd.DataFrame(data)
+df.to_csv('hometowns.csv')
