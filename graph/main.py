330a30a68db2602b19324898713d01f5a30cda11
lukezann <luke.zanuck@gmail.com>
2024-01-10 00:19:57 -0500

    Use greedy TSP to find path. Refactor code for cleanliness.

--- graph/main.py
+++ graph/main.py
@@ -1,14 +1,26 @@
 import pandas as pd
-from trmap import TRMap, makeGraph, findBestPath
+import trmap as tr
+import matplotlib.pyplot as plt
 
 # read in csv
 df = pd.read_csv('/Users/lukezanuck/Desktop/Projects/traveling-runner/python-roster-scraper/hometowns.csv')
 df = df.drop(labels='Unnamed: 0', axis=1)
+#df = df.drop_duplicates(subset="hometown")
+df = df.drop(index=7)
+print(df)
 
 starting_loc = 'Los Angeles, Calif.'
 current_loc = starting_loc
 ending_loc = 'Williams College'
 goal_mileage = 3500
-full_graph = makeGraph(df)
-shortest_path = findBestPath(full_graph, starting_loc, ending_loc)
-print(shortest_path)
+G = tr.makeGraph(df)
+
+# tsp
+greedy_tsp = tr.nx.approximation.greedy_tsp
+path = greedy_tsp(G, source=starting_loc)
+path.pop(len(path)-1)
+print(path)
+
+# code to draw graph
+# tr.nx.draw(full_graph)
+# plt.show()
--- graph/trmap.py
+++ graph/trmap.py
@@ -6,7 +6,7 @@ from haversine import Unit
 
 class TRMap:
 
-    def __init__(self, graph: nx.Graph, 
+    def __init__(self, graph: nx.Graph(), 
                 current_mileage: int, 
                 goal_mileage: int, 
                 starting_loc: str,
@@ -26,29 +26,27 @@ class TRMap:
     def updateGoalMileage(self, new_goal_mileage: int):
         self.goal_mileage = new_goal_mileage
 
-def makeGraph(data: pd.DataFrame) -> nx.Graph:
+def makeGraph(data: pd.DataFrame) -> nx.Graph():
         # create empty graph
-        graph = nx.Graph
+        G = nx.Graph()
 
         # iterate through every possible combination
-        for i in range(0, len(data)+1):
-            for j in range(0, len(data)+1):
+        for i in range(0, len(data)):
+            city = data.iloc[i]['hometown']
+            city_coords = (data.iloc[i]['latitude'], data.iloc[i]['longitude'])
 
-                # do not need to make a self-loop
-                if i == j: break
+            G.add_node(city)
 
-                city1_name = data.iloc[i]['hometown']
-                city2_name = data.iloc[j]['hometown']
+            for j in range(0, len(data)):
 
-                city1_coords = (data.iloc[i]['latitude'], data.iloc[i]['longitude'])
-                city2_coords = (data.iloc[j]['latitude'], data.iloc[j]['longitude'])
-
-                distance = round(hs.haversine(city1_coords, city2_coords, unit=Unit.MILES), 2)
+                if i is not j:
+                    dest = data.iloc[j]['hometown']
+                    dest_coords = (data.iloc[j]['latitude'], data.iloc[j]['longitude'])
+                    distance = round(hs.haversine(city_coords, dest_coords, unit=Unit.MILES), 2)
+                    G.add_edge(city, dest, weight=distance)
                 
-                graph.add_edge(city1_name, city2_name, weight=distance)
-
-        return graph
+        return G
     
-# find path closest to mileage goal with most amount of stops
-def findBestPath(graph: nx.Graph, start: str, end: str):
-    pass
+#TODO: helper function to find distance between two locations
+def dist(data, city1: str, city2: str):
+    pass   
